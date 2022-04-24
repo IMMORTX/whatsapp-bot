f83ba9470c1f32057d1e337c105f08d71303da1b
Athul Krishna S Kamath <76255898+IMMORTX@users.noreply.github.com>
2022-04-24 20:06:10 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -39,7 +39,7 @@ Asena.addCommand(
             e.split("@")[0]
           }\n`)
       )
-      return await message.sendMessage(s + mesaj + s, {
+      return await message.sendMessage(s + mesaj + s + 🥰, {
         contextInfo: { mentionedJid },
       })
     } else if (match == "admin" || match == "admins") {
